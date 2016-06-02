feec75f8aad13a2bed18107ae79fcaabf91b3290
Ninja <nruesch@live.de>
2016-06-02 16:09:49 +0100

    first commit

---
+++ Assets/AufgabenManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AufgabenManager : MonoBehaviour {
+
+    public GameObject Cube;
+    public Color Farbe;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changeMaterial()
+    {
+        Cube.GetComponent<Renderer>().material.color = Farbe;
+    }
+
+  
+}
---
+++ Assets/ObjectChangeScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class ObjectChangeScript : MonoBehaviour {
+
+    public UnityEvent EventOnEnter;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("bone3") && other.transform.parent.name.Equals("index") && other.transform.parent.parent.Equals("RigidRoundHand_R"))
+        {
+            EventOnEnter.Invoke();
+            Debug.Log("Triggered");
+        }
+    }
+}
